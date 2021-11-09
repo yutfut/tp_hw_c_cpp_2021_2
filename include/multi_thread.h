b38a8cf700c9fbbad1692a78a8cce8e954244142
yut_fut <seregey.golubev@mail.ru>
2021-11-09 17:08:24 +0300

    edited header

--- include/multi_thread.h
+++ include/multi_thread.h
@@ -1,7 +1,7 @@
 // Copyright 2021 Golubev
 
-#ifndef TP_HW_C_CPP_2021_2_MULTI_THREAD_HEADER_H_
-#define TP_HW_C_CPP_2021_2_MULTI_THREAD_HEADER_H_
+#ifndef TP_HW_C_CPP_2021_2_INCLUDE_MULTI_THREAD_H_
+#define TP_HW_C_CPP_2021_2_INCLUDE_MULTI_THREAD_H_
 
 #include <stdio.h>
 #include <unistd.h>
@@ -16,4 +16,4 @@ void* check(void *args);
 
 int counter(const char *file_name);
 
-#endif //TP_HW_C_CPP_2021_2_MULTI_THREAD_HEADER_H_
+#endif //TP_HW_C_CPP_2021_2_INCLUDE_MULTI_THREAD_H_
