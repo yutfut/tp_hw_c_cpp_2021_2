23ab83626ce56c644745bc33fdd0d0b1e621dabc
yut_fut <seregey.golubev@mail.ru>
2021-11-09 17:06:35 +0300

    edited headers

--- include/one_thread.h
+++ include/one_thread.h
@@ -1,9 +1,10 @@
 // Copyright 2021 Golubev
 
-#ifndef TP_HW_C_CPP_2021_2_INCLUDE_MULTI_THREAD_H_
+#ifndef TP_HW_C_CPP_2021_2_ONE_THREAD_H_
+#define TP_HW_C_CPP_2021_2_ONE_THREAD_H_
 
 #include <stdio.h>
 
 int counter(const char *file_name);
 
-#endif //TP_HW_C_CPP_2021_2_ONE_THREAD_H
+#endif //TP_HW_C_CPP_2021_2_ONE_THREAD_H_
